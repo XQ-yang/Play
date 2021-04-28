1f8f12ac7f10d6a5aae296021390d12cad6f0c9a
“XQ-Yang” <3267568158@qq.com>
2021-04-28 15:55:44 +0800

    update

---
+++ src/usedClass/Demo04.java
@@ -0,0 +1,13 @@
+package usedClass;
+
+/**
+ * @Author: XQ-Yang
+ * @Date: 2021/4/28 0028
+ * @Tools: IntelliJ IDEA
+ * @Remember: Be professional every day
+ **/
+public class Demo04 {
+    public static void main(String[] args) {
+        System.out.println(Math.PI);
+    }
+}
