b50940423c075f62fd1cd156c6fe10c98f6d6f3f
“XQ-Yang” <3267568158@qq.com>
2021-04-30 20:07:37 +0800

    update

---
+++ src/IO/Demo03.java
@@ -0,0 +1,15 @@
+package IO;
+
+/**
+ * @Author: XQ-Yang
+ * @Date: 2021/4/30 0030
+ * @Tools: IntelliJ IDEA
+ * @Remember: Be professional every day
+ **/
+public class Demo03 {
+    public static void main(String[] args) {
+        int a = 0b10;
+        System.out.println(a);
+
+    }
+}
